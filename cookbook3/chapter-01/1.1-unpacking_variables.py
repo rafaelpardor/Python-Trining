4a2271fd7d0bb19534dec3ce79c2c0e32a2154cd
rafaelpardor <rafaelpardor@protonmail.com>
2019-12-17 00:46:43 -0500

    Unpacking varialbes

--- cookbook3/chapter-01/1.1-unpacking_variables.py
+++ cookbook3/chapter-01/1.1-unpacking_variables.py
@@ -1,18 +1,24 @@
+#!/usr/bin/python3
+
+# You have an N-element tuple or sequence that you would like to unpack into collection of N varialbes
 print("""
 Any sequence can be unpacked into variables using assigment operation.
+The only req. is that the number of variables and structure match the sequence.
         """)
 p = (4, 5)
 x, y = p
 
-print('X: ',x)
-print('Y: ', y)
+print("var x: {}".format(x))
+print("var y: {}".format(y))
 
 
+# When unpacking, yoou may sometins want to discard certain values.
+# Python has no speacial syntax for this.
 data = ['ACME','Nuclear bomb', 50, 91.1, (2012,12,21)]
-name, product, ammount, _, date = data
+name, product, *_, (*_, day) = data
 
-print("Data: ", data)
-print(name, product)
+print("Data values: {}".format(data))
+print("Name: ",name,"\nProduct: ", product, "\nDay: ", day)
 
 print("""
         Unpacking works with any object that hapens to be iterable,
---
+++ cookbook3/chapter-01/1.2-unpacking_iterables.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+# We need to unpacl N elements from an iterable, but the iterable has to many values
+print("""
+Python 'star expressions' can be used to recollect too many values to unpack.
+        """)
+
+record = ('Rafael', 'rafael@example.com','312312312','31321312312', 20)
+name, email, *phone_numbers, age= record
+
+# The phone_numbers variable will always be a list, regardless of how many phone numbers are unpacked
+print(phone_numbers,"\n")
+
+print("""
+        Extended iterable unpacking is tailor-made for unpacking iterables of unknown or arbitrary length,
+        Star unpacking lets the developer leverage those patterns easily instead of performing acrobatics
+        to get at the relevant elements in the iterable.
+        """)
+
+records = [
+        ('foo', 1, 2),
+        ('bar', 'hello'),
+        ('foo', 3, 4),
+        ]
+
+def do_foo(x, y):
+    print('this is foo ->', x, y)
+
+def do_bar(s):
+    print('this is bar ->', s)
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
+
+items = [1,2,3,4,5,6]
+head, *tail = items
+print("\n", head)
+print(tail)
+
+def sum(items):
+    head, *tail = items
+    return head + sum(tail) if tail else head
+
+print(sum(items))
+
