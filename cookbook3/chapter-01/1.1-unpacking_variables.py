c03cc3703999a876662ecab45d23df45e8d410cc
rafaelpardor <rafaelpardor@protonmail.com>
2020-01-21 10:28:07 -0500

    Python cookbook

--- cookbook3/chapter-01/1.1-unpacking_variables.py
+++ cookbook3/chapter-01/1.1-unpacking_variables.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-# You have an N-element tuple or sequence that you would like to unpack into collection of N varialbes
+# You have an N-element tuple or sequence that you would like to unpack into collection of N variables.
 print("""
 Any sequence can be unpacked into variables using assigment operation.
-The only req. is that the number of variables and structure match the sequence.
+The only requirement is that the number of variables and structure match the sequence.
         """)
 p = (4, 5)
 x, y = p
@@ -12,17 +12,19 @@ print("var x: {}".format(x))
 print("var y: {}".format(y))
 
 
-# When unpacking, yoou may sometins want to discard certain values.
+# When unpacking, you may sometimes want to discard certain values.
 # Python has no speacial syntax for this.
 data = ['ACME','Nuclear bomb', 50, 91.1, (2012,12,21)]
 name, product, *_, (*_, day) = data
 
 print("Data values: {}".format(data))
-print("Name: ",name,"\nProduct: ", product, "\nDay: ", day)
+print("Name:",name,"\nProduct:", product, "\nDay:", day)
+
 
 print("""
-        Unpacking works with any object that hapens to be iterable,
+        Unpacking works with any object that happens to be iterable,
         not just tuples or list.
         Includes strings, iterators and generators
         """)
+
 
--- cookbook3/chapter-01/1.2-unpacking_iterables.py
+++ cookbook3/chapter-01/1.2-unpacking_iterables.py
@@ -9,7 +9,8 @@ record = ('Rafael', 'rafael@example.com','312312312','31321312312', 20)
 name, email, *phone_numbers, age= record
 
 # The phone_numbers variable will always be a list, regardless of how many phone numbers are unpacked
-print(phone_numbers,"\n")
+print("record:", record)
+print("phone number:", phone_numbers,"\n")
 
 print("""
         Extended iterable unpacking is tailor-made for unpacking iterables of unknown or arbitrary length,
@@ -17,11 +18,13 @@ print("""
         to get at the relevant elements in the iterable.
         """)
 
-records = [
+foo_bar = [
         ('foo', 1, 2),
         ('bar', 'hello'),
         ('foo', 3, 4),
-        ]
+    ]
+
+print(foo_bar)
 
 def do_foo(x, y):
     print('this is foo ->', x, y)
@@ -29,7 +32,7 @@ def do_foo(x, y):
 def do_bar(s):
     print('this is bar ->', s)
 
-for tag, *args in records:
+for tag, *args in foo_bar:
     if tag == 'foo':
         do_foo(*args)
     elif tag == 'bar':
