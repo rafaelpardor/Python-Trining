8bdea90b018d973b8141ad34a4d77ee8513f96b5
rafael <rafaelpardor@protonmail.com>
2020-03-20 22:33:47 -0500

    Numbers

---
+++ python/01-data-types/01-numbers/01-operators.py
@@ -0,0 +1,19 @@
+# !/usr/bin/python3
+
+def operators():
+    print('--- Numbers Operators ---')
+
+    print('Add:', 2+2)
+    print('Adding Descimal Numbers:', 1.2+3.843)
+    print('Subtraction:', 4-85)
+    print('Divide:', 4/8)
+    print('Divide int:', 10//3)
+    print('Divide, floats:', 5.15/4.9)
+    print('Module:', 9 % 2)
+    print('Potentiation:', 4**5)
+    print('PEMDAS 4+5*(45-65)+54/(45+98+(4**5)):',
+          4+5*(45-65)+54/(45+98+(4**5)))
+
+
+if __name__ == "__main__":
+    operators()
---
+++ python/01-data-types/01-numbers/03-float.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python3
+import math
+
+
+def float_numbers():
+    print('--- Float Numbers ---')
+    seno = math.sin(20)
+
+    print('SIN: {}, Type: {}'.format(seno, type(seno)))
+
+
+if __name__ == "__main__":
+    float_numbers()
